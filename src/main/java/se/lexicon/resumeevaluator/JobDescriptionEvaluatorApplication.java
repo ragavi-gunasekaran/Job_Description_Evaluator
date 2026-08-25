c0a459f2caed0edbe5d609d220363d5aa7055a39
Ragavi Gunasekaran <ragavigunasekaran@gmail.com>
2026-08-25 14:02:11 +0200

    Initial Spring Boot setup

---
+++ src/main/java/se/lexicon/resumeevaluator/JobDescriptionEvaluatorApplication.java
@@ -0,0 +1,12 @@
+package se.lexicon.resumeevaluator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobDescriptionEvaluatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JobDescriptionEvaluatorApplication.class, args);
+    }
+}
